28ec34602bf5e21e7416164f25870f8c3bab974c
sola1408 <100016401+sola1408@users.noreply.github.com>
2022-10-14 23:30:38 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,5 +4,5 @@ let getRandom = function (min, max){
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min)) + min;
     }
-    return throwNewError("Неправильный диапазон")
+    return throw new Error("Неправильный диапазон")
 }
