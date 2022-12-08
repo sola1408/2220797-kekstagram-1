d0b88d84b078c0c862c3d36762a63e281a9703a6
sola1408 <100016401+sola1408@users.noreply.github.com>
2022-12-08 10:32:16 +0500

    module12-task1 + other ready files

--- js/main.js
+++ js/main.js
@@ -1,24 +1,14 @@
-// import { commentTexts, names } from "./data/names.js";
-// import { IPhotoDescription, IComment } from "./interfaces.js";
-// import { getPhotoDescriptions } from "./photo.js";
-// import { getRandomNumber, getUniqueRandomNumber } from "./random.js";
-// import { checkString } from "./utils.js";
-// import './photoRender.js'
-import './form.js';
-// const randomCount: number = 25;
-// const maxLenght: number = 10;
-// const str: string = 'В этой строке точно больше 10 символов';
-// const min: number = 12;
-// const max: number = 20;
-// const photoDescriptions = getPhotoDescriptions(randomCount);
-// console.log(photoDescriptions);
-// const isCorrectly: boolean = checkString(str, maxLenght);
-// console.log(isCorrectly);
-// const randomNumber: number = getRandomNumber(min, max);
-// console.log('Случайное число от ' + min + ' до ' + max + ' = ' + randomNumber);
-// const uniqueRandomNumbers: number[] = [];
-// for (let i = 0; i < 7; i++) {
-//     const uniqueRandomNumber: number = getUniqueRandomNumber(min, max);
-//     uniqueRandomNumbers.push(uniqueRandomNumber);
-// }
-// console.log('Случайные и уникальные числа от ' + min + ' до ' + max + ' = ' + uniqueRandomNumbers);
+import {renderUserPhotos} from './pictures.js';
+import {initEffects} from './effects.js';
+import {closeUploadPopup} from './form.js';
+import {submitForm} from './form.js';
+import {getData} from './api.js';
+import {showFilteredPictures} from './filter.js';
+
+getData((pictures) => {
+  renderUserPhotos(pictures);
+  showFilteredPictures(pictures);
+});
+
+submitForm(closeUploadPopup);
+initEffects();
